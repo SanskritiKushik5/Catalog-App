ef53851cc3dc8f983c219bb2c39585782aca7395
SanskritiKushik5 <sanskritikushik23@gmail.com>
2021-11-16 19:54:27 +0530

    Sign in anonymously

---
+++ lib/services/auth.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Anonymously
+  Future signInAnon() async {
+    try {
+      AuthResult result = await _auth.signInAnonymously();
+      FirebaseUser user = result.user;
+      return user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // Email and Pwd
+
+  // Registering
+
+  // Sign out
+}
