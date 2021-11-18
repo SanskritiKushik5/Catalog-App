52b10fe6882870c89d8765f90e7f8e56fe559c1e
SanskritiKushik5 <sanskritikushik23@gmail.com>
2021-11-18 17:57:47 +0530

    Basic setup

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Catalog App"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Container(
+          child: const Text('Welcome'),
+        ),
+      ),
+      drawer: const Drawer(),
+    );
+  }
+}
